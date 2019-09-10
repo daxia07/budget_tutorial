ed532df05a8ae5ef18db90a8376c3ffdb8029a19
alienz <limingxia07@gmail.com>
2019-09-10 18:46:47 +1000

    ui add element

--- app.js
+++ app.js
@@ -5,24 +5,24 @@ var budgetModel = (() => {
     totalExpense: 0,
     totalIncome: 0
   }
-
   class Expense {
     constructor(des, value) {
       this.des = des;
       this.value = value;
     };
   }
-
   class Income {
     constructor(des, value) {
       this.des = des;
       this.value = value;
     };
   }
-
   const addElement = (type, des, value) => {
     var item, id;
-    incomeList = data.incomeList;
+    let {
+      incomeList,
+      expenseList
+    } = data;
     if (type === 'inc') {
       item = new Income(des, value);
       if (incomeList.length > 0) {
@@ -46,6 +46,7 @@ var budgetModel = (() => {
     } else {
       throw new Error('type not expected!')
     }
+    return id;
   };
 
   return {
@@ -56,18 +57,80 @@ var budgetModel = (() => {
 
 
 var budgetUI = (() => {
-  return {
-    updateUI: () => {
-      console.log('updated')
+  const DOM = {
+    input: {
+      type: '.add__type',
+      des: '.add__description',
+      value: '.add__value',
     },
+    incomeList: '.income__list',
+    expenseList: '.expenses__list',
+    item_html: ({
+      type,
+      id,
+      des,
+      value
+    }) => `<div class="item clearfix" id="${type}-${id}"><div class="item__description">${des}</div><div class="right clearfix"><div class="item__value">${value}</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>`
+  };
+
+  const renderElement = (type, des, value, id) => {
+    let prefix, targetEle, new_item;
+    if (type === 'inc') {
+      prefix = 'income';
+      targetEle = document.querySelector(DOM.incomeList);
+    } else if (type === 'exp') {
+      prefix = 'expense';
+      targetEle = document.querySelector(DOM.expenseList);
+    } else {
+      throw new Error('unexpected type!')
+    }
+    new_item = DOM.item_html({
+      type,
+      value,
+      des,
+      id
+    });
+    targetEle.insertAdjacentHTML('afterbegin', new_item);
+  };
+
+
+  return {
+    updateUI: (type, des, value) => {
+      console.log(`updated entry for ${type} ${des} ${value}`)
+    },
+    DOM,
+    renderElement,
   }
 })();
 
 
 var controller = (model, ui) => {
+  DOM = ui.DOM;
+  const addItem = () => {
+    let type, des, value;
+    type = document.querySelector(DOM.input.type).value;
+    des = document.querySelector(DOM.input.des).value;
+    value = parseFloat(document.querySelector(DOM.input.value).value);
+    id = model.addElement(type, des, value);
+    ui.renderElement(type, des, value, id);
+    console.log('item added!');
+    console.log(model.data)
+    // fetch value and add to model, update UI
+  };
+
+  const setupListeners = () => {
+    document.querySelector('.add__btn').addEventListener('click', addItem);
+    document.addEventListener('keypress', e => {
+      if (e.keyCode === 13) {
+        addItem();
+      }
+    })
+  };
+
   return {
     init: () => {
       console.log('Application launched!');
+      setupListeners();
       // change display to default
       // add listeners
     },
@@ -79,5 +142,7 @@ var controller = (model, ui) => {
 
 ctl = controller(budgetModel, budgetUI);
 ctl.init();
-ctl.publicTestAddElement('inc', 'wd', 2000);
-console.log(ctl.publicTestData.totalIncome);
+
+// TEST
+// ctl.publicTestAddElement('inc', 'wd', 2000);
+// console.log(ctl.publicTestData.totalIncome);
